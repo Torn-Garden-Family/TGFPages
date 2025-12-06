f8a5d76d466bf654f315dd9e0754af6a4c87a874
Scatterbeans <scatterbeaned@gmail.com>
2025-12-06 01:00:08 +0100

    Update branding and graphics paths for site rename

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 function Main() {
-    if (window.location.href === "https://torn-garden-family.github.io/TGFPages/") {
-        window.location.href = "https://torn-garden-family.github.io/TGFPages/main.html";
+    if (window.location.href === "https://torn-community-garden.github.io/pages/") {
+        window.location.href = "https://torn-community-garden.github.io/pages/main.html";
     }
 }
